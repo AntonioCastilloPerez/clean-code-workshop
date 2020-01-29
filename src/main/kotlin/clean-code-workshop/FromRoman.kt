a7266b228e2bf68a72fcbb33ae84cf6cd378c240
Antonio Castillo Perez <antonio.castillo@my-rio.de>
2020-01-29 10:27:16 +0100

    Refactor code

--- src/main/kotlin/clean-code-workshop/FromRoman.kt
+++ src/main/kotlin/clean-code-workshop/FromRoman.kt
@@ -2,7 +2,26 @@ package `clean-code-workshop`
 
 class FromRoman {
 
-    private val romanMap = mapOf<String, Int>((
+    fun fromRoman(romanString: String): Any {
+        return try {
+            val mappedRomanNumber = mapFromRomanLetterToNumber(romanString)
+            val negatedRomanNumber = negate(mappedRomanNumber)
+            negatedRomanNumber.sum()
+
+        } catch (e: NullPointerException) {
+            "Uknown roman number contained in the character"
+        }
+    }
+
+    private fun negate(input: List<Int>): List<Int> {
+        return input.zipWithNext { a, b -> if (a < b) { a * -1 } else a }.plus(input.last())
+    }
+
+    private fun mapFromRomanLetterToNumber(romanChar: String): List<Int> {
+        return romanChar.chunked(1).map { x -> romanMap[x]!! }
+    }
+
+    private val romanMap = mapOf((
             ("I" to 1)),
             ("V" to 5),
             ("X" to 10),
@@ -12,29 +31,5 @@ class FromRoman {
             ("M" to 1000)
     )
 
-    fun fromRoman(romanString: String): Any {
-        return try {
-            val mappedRomanNumber = mapFromRomanLetterToNumber(romanString)
-            val negatedRomanNumber = negateArray(mappedRomanNumber)
-            negatedRomanNumber.sum()
-
-        } catch (e: NullPointerException) {
-            "Uknown roman number contained in the character"
-        }
-
-    }
-
-    fun negateArray(input: List<Int>): List<Int> {
-        return input.zipWithNext { a, b ->
-            if (a < b) {
-                a * -1
-            } else a
-        }.plus(input.last())
-    }
-
-    fun mapFromRomanLetterToNumber(romanChar: String): List<Int> {
-        val singleRomanNumbers = romanChar.chunked(1)
-        return singleRomanNumbers.map { x -> romanMap[x]!! }
-    }
 
 }
--- src/test/kotlin/clean-code-workshop/FromRomanTest.kt
+++ src/test/kotlin/clean-code-workshop/FromRomanTest.kt
@@ -7,54 +7,22 @@ import org.junit.jupiter.api.Test
 class FromRomanTest {
 
     @Test
-    fun `negate roman list`() {
-
-        val cleanCodeTrainingTest = FromRoman().negateArray(listOf(1, 2, 3, 4))
-
-        Assertions.assertThat(cleanCodeTrainingTest).isEqualTo((listOf(-1, -2, -3, 4)))
-    }
-
-    @Test
-    fun `map char of roman letter to numbers`() {
-
-        val cleanCodeTrainingTest = FromRoman().mapFromRomanLetterToNumber("IVXLCDM")
-
-        Assertions.assertThat(cleanCodeTrainingTest).isEqualTo((listOf(1, 5, 10, 50, 100, 500, 1000)))
-    }
-
-    @Test
     fun `check correct transformation of roman number with just one character`() {
-
-        val cleanCodeTrainingTest = FromRoman().fromRoman("I")
-
-        Assertions.assertThat(cleanCodeTrainingTest).isEqualTo(1)
-
+        Assertions.assertThat(FromRoman().fromRoman("I")).isEqualTo(1)
     }
 
     @Test
     fun `check correct tranformation of roman number with just two characters`() {
-
-        val cleanCodeTrainingTest = FromRoman().fromRoman("IV")
-
-        Assertions.assertThat(cleanCodeTrainingTest).isEqualTo(4)
-
+        Assertions.assertThat(FromRoman().fromRoman("IV")).isEqualTo(4)
     }
 
     @Test
     fun `test behaviour with unknown letter`() {
-
-        val cleanCodeTrainingTest = FromRoman().fromRoman("VIO")
-
-        Assertions.assertThat(cleanCodeTrainingTest).isEqualTo("Uknown roman number contained in the character")
-
+        Assertions.assertThat(FromRoman().fromRoman("VIO")).isEqualTo("Uknown roman number contained in the character")
     }
 
     @Test
     fun `check correct transformation of roman number`() {
-
-        val cleanCodeTrainingTest = FromRoman().fromRoman("MCMXLV")
-
-        Assertions.assertThat(cleanCodeTrainingTest).isEqualTo(1945)
-
+        Assertions.assertThat(FromRoman().fromRoman("MCMXLV")).isEqualTo(1945)
     }
 }
