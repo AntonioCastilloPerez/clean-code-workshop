4833752ac0c91bf569ac6eb2008362c477d66dc8
Antonio Castillo Perez <antonio.castillo@my-rio.de>
2020-02-15 14:47:32 +0100

    Add text Wrapping exercise

---
+++ src/main/kotlin/clean-code-workshop/TextWrapping.kt
@@ -0,0 +1,64 @@
+package `clean-code-workshop`
+
+class TextWrapping {
+
+    fun Umbrechen(text: String, maxLineLength: Int, indent: Boolean): List<String> {
+
+        val brakedDownText = textSplitAndCount(text, maxLineLength)
+        val formatted = formatLines(brakedDownText, maxLineLength)
+
+        return if (indent) justifyLines(formatted, maxLineLength) else formatted
+    }
+
+    private fun justifyLines(formatted: List<String>, maxLineLength: Int): List<String> {
+        val numOfCharsInText = formatted.fold(0) { sum, element -> sum.plus(element.length) }
+        val numberOfExpectedCharsInText = formatted.size * maxLineLength
+
+        return if (numOfCharsInText != numberOfExpectedCharsInText) {
+            justifyLines(formatted.map {
+                when {
+                    it.length < maxLineLength -> addBlanks(it, maxLineLength)
+                    it.length > maxLineLength -> subtractBlanks(it, maxLineLength)
+                    else -> it
+                }
+            }, maxLineLength)
+        } else return formatted
+    }
+
+    private fun subtractBlanks(input: String, maxLineLength: Int): String {
+        return if (maxLineLength < input.length) {
+            subtractBlanks(input.replaceFirst(" ", ""), maxLineLength)
+        } else return input
+    }
+
+    private fun addBlanks(input: String, maxLineLength: Int): String {
+        return if (maxLineLength > input.length) {
+            addBlanks(input.replace(" ", "  "), maxLineLength)
+        } else return input
+    }
+
+
+    fun textSplitAndCount(text: String, maxLineLength: Int): List<Pair<String, Int>> {
+        return text.split(" ").map { it.chunked(maxLineLength) }.flatten().map { it to it.length }
+    }
+
+    fun formatLines(input: List<Pair<String, Int>>, maxLineLength: Int): List<String> {
+        var countLines = 0
+        val formattedList = mutableListOf("")
+
+        input.map { it }.fold(0, { total, next ->
+            if (total.plus(next.second + 1) > maxLineLength) {
+                countLines += 1
+                formattedList.add(countLines, next.first)
+                next.second
+            } else {
+                formattedList[countLines] = formattedList[countLines] + " " + next.first
+                total.plus(next.second + 1)
+            }
+        })
+
+        return formattedList.map { it.trimIndent() }
+    }
+
+
+}
---
+++ src/test/kotlin/clean-code-workshop/TextWrappingTest.kt
@@ -0,0 +1,112 @@
+package `clean-code-workshop`
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class TextWrappingTest {
+    @Test
+    fun `from a string create a list with every element`() {
+
+        val input = "Es blaut die Nacht,"
+
+        val expectedOutput = listOf(
+                Pair("Es", 2),
+                Pair("blaut", 5),
+                Pair("die", 3),
+                Pair("Nacht,", 6)
+        )
+
+        Assertions.assertThat(TextWrapping().textSplitAndCount(input, maxLineLength = 9)).isEqualTo(expectedOutput)
+
+    }
+
+    @Test
+    fun `from a string create a list with every element with words larger than the max length`() {
+
+        val input = "Es blaut die Nacht,"
+
+        val expectedOutput = listOf(
+                Pair("Es", 2),
+                Pair("bla", 3),
+                Pair("ut", 2),
+                Pair("die", 3),
+                Pair("Nac", 3),
+                Pair("ht,", 3)
+        )
+
+        val maxLength = 3
+
+        Assertions.assertThat(TextWrapping().textSplitAndCount(input, maxLength)).isEqualTo(expectedOutput)
+
+    }
+
+    @Test
+    fun `create List with output lines`() {
+
+        val input = listOf(
+                Pair("Es", 2),
+                Pair("blaut", 5),
+                Pair("die", 3),
+                Pair("Nacht,", 6),
+                Pair("die", 3),
+                Pair("Sternlein", 9),
+                Pair("blinken,", 8)
+        )
+        val maxlength = 9
+
+        val expectedOutput = listOf(
+                "Es blaut",
+                "die",
+                "Nacht,",
+                "die",
+                "Sternlein",
+                "blinken,"
+        )
+        Assertions.assertThat(TextWrapping().formatLines(input, maxlength)).isEqualTo(expectedOutput)
+
+    }
+
+    @Test
+    fun `format a String without indentation with maximum length`() {
+
+        val input = "Es blaut die Nacht, " +
+                "die Sternlein blinken, " +
+                "Schneeflöcklein leis hernieder sinken."
+        val maxlength = 9
+
+        val expectedOutput = listOf(
+                "Es blaut",
+                "die",
+                "Nacht,",
+                "die",
+                "Sternlein",
+                "blinken,",
+                "Schneeflö",
+                "cklein",
+                "leis",
+                "hernieder",
+                "sinken."
+        )
+        Assertions.assertThat(TextWrapping().Umbrechen(input, maxlength, false)).isEqualTo(expectedOutput)
+
+    }
+
+    @Test
+    fun `format a String with justification`() {
+
+        val input = "Es blaut die Nacht, " +
+                "die Sternlein blinken, " +
+                "Schneeflöcklein leis hernieder sinken."
+        val maxlength = 22
+
+        val expectedOutput = listOf(
+                "Es  blaut  die  Nacht,",
+                "die Sternlein blinken,",
+                "Schneeflöcklein   leis",
+                "hernieder      sinken."
+        )
+        Assertions.assertThat(TextWrapping().Umbrechen(input, maxlength, true)).isEqualTo(expectedOutput)
+
+    }
+
+}
