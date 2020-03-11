0a9fba28b5e37302adaf6d77ecf3bb22d442b0ba
Antonio Castillo Perez <antonio.castillo@my-rio.de>
2020-03-11 23:22:57 +0100

    Refactor

--- src/main/kotlin/clean-code-workshop/CalculateNextGeneration.kt
+++ src/main/kotlin/clean-code-workshop/CalculateNextGeneration.kt
@@ -4,6 +4,17 @@ import java.io.File
 
 typealias booleanMatrix = Array<Array<Boolean>>
 
+data class CellStatus(
+        val alive: Boolean = true,
+        val dead: Boolean = false
+)
+
+data class Operators(
+        val equalTo: Int = 0,
+        val lowerThan: Int = -1,
+        val greaterThan: Int = 1
+)
+
 class CalculateNextGeneration {
 
     fun calculateNextGeneration(path: String) {
@@ -21,26 +32,26 @@ class CalculateNextGeneration {
         return txt.map { it -> it.chunked(1).map { it == "*" }.toTypedArray() }.toTypedArray()
     }
 
-    fun booleanMatrix.booleanMatrixToFile() {
-        val toPrint = this.map { it -> it.map { if (it) "*" else "." }.joinToString("") }.joinToString(separator = "\n")
+    private fun booleanMatrix.booleanMatrixToFile() {
+        val toPrint = this.joinToString(separator = "\n") { it -> it.joinToString("") { if (it) "*" else "." } }
         File("src/test/resources/output.txt").writeText(toPrint)
     }
 
     fun booleanMatrix.killOverPopulatedCells(): booleanMatrix {
-        return operateOnMatrix(this, 1, 3, Pair(false, "kill"))
+        return operateOnMatrix(this, Operators().greaterThan, 3, Pair(CellStatus().dead, "kill"))
     }
 
     fun booleanMatrix.killUnderpopulatedCells(): booleanMatrix {
-        return operateOnMatrix(this, -1, 2, Pair(false, "kill"))
+        return operateOnMatrix(this, Operators().lowerThan, 2, Pair(CellStatus().dead, "kill"))
     }
 
-    fun booleanMatrix.permitCellLive(numbersToPermit: Pair<Int, Int>): booleanMatrix {
-        val output = operateOnMatrix(this, 0, numbersToPermit.first, Pair(true, "permit"))
-        return operateOnMatrix(output, 0, numbersToPermit.second, Pair(true, "permit"))
+    fun booleanMatrix.permitCellLive(limitsToPermit: Pair<Int, Int>): booleanMatrix {
+        val output = operateOnMatrix(this, Operators().equalTo, limitsToPermit.first, Pair(CellStatus().alive, "permit"))
+        return operateOnMatrix(output, Operators().equalTo, limitsToPermit.second, Pair(CellStatus().alive, "permit"))
     }
 
     fun booleanMatrix.resurrectCells(): booleanMatrix {
-        return operateOnMatrix(this, 0, 3, Pair(true, "resurrect"))
+        return operateOnMatrix(this, Operators().equalTo, 3, Pair(CellStatus().alive, "resurrect"))
     }
 
     private fun operateOnMatrix(matrix: booleanMatrix, operator: Int, limit: Int, killOrResurrect: Pair<Boolean, String>): booleanMatrix {
@@ -50,19 +61,23 @@ class CalculateNextGeneration {
         for (row in 0 until rows) {
             for (col in 0 until columns) {
                 val numberOfLivingNeighbours = countLivingNeighboursForCell(matrix, row, col)
-                if (numberOfLivingNeighbours.compareTo(limit) == operator) {
-                    if (operator == 0 && killOrResurrect.second == "permit") {
-                        matrix[row][col] = matrix[row][col]
-                    } else {
-                        matrix[row][col] = killOrResurrect.first
-                    }
-                } else {
-                    matrix[row][col] = matrix[row][col]
-                }
+                decideMatrixCellStatus(numberOfLivingNeighbours, limit, operator, killOrResurrect, matrix, row, col)
             }
         }
 
         return matrix
+    }
+
+    private fun decideMatrixCellStatus(numberOfLivingNeighbours: Int, limit: Int, operator: Int, killOrResurrect: Pair<Boolean, String>, matrix: booleanMatrix, row: Int, col: Int) {
+        if (numberOfLivingNeighbours.compareTo(limit) == operator) {
+            if (operator == 0 && killOrResurrect.second == "permit") {
+                matrix[row][col] = matrix[row][col]
+            } else {
+                matrix[row][col] = killOrResurrect.first
+            }
+        } else {
+            matrix[row][col] = matrix[row][col]
+        }
     }
 
     fun countLivingNeighboursForCell(matrix: booleanMatrix, row: Int, col: Int): Int {
